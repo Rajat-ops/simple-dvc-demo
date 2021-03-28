723885de2dbf24e2953d5f3239a17aa42b88c766
Rajat <jraj8407@gmail.com>
2021-03-28 18:19:55 +0530

    set up done

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "src", 
+    version = "0.0.1",
+    description = "its MLOPS on WineQ",
+    author = "Rajat",
+    packages = find_packages(),
+    license = "MIT",
+    
+)
--- src/load_data.py
+++ src/load_data.py
@@ -1,6 +1,19 @@
 import os
-from get_data import get_data, read_params
+import yaml
+import pandas as pd
 import argparse
+
+def read_params(config_path):
+    with open(config_path) as yaml_file:
+        config = yaml.safe_load(yaml_file)
+    return config
+
+def get_data(config_path):
+    config = read_params(config_path)
+    data_path = config["data_source"]['s3_source']
+    
+    df = pd.read_csv(data_path, sep=",", encoding='utf-8')
+    return df
 
 def load_and_save(config_path):
     config = read_params(config_path)
@@ -14,4 +27,5 @@ if __name__=="__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--config", default="params.yaml")
     parsed_args = args.parse_args()
+    data = get_data(config_path = parsed_args.config)
     load_and_save(config_path=parsed_args.config)
